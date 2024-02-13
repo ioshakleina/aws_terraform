5d7ea547bfa1e0515dcca77e1e616510a270d956
ioshakleina <ioshakleina@gmail.com>
2024-02-13 23:25:43 +0200

    one_solution

--- main.tf
+++ main.tf
@@ -1,1 +1,142 @@
 
+provider "aws" {
+  region = var.Region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = var.cidr
+  instance_tenancy = "default"
+
+  tags = {
+    Name        = "MainVPC"
+    Environment = var.Environment
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.publicCIDR[0]
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "PublicSubnet"
+    Environment = var.Environment
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "InternetGateway"
+    Environment = var.Environment
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name        = "PublicRouteTable"
+    Environment = var.Environment
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2_sg"
+  description = "Security group for EC2 instance"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = var.allowed_ports[0]
+    to_port     = var.allowed_ports[0]
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = var.allowed_ports[1]
+    to_port     = var.allowed_ports[1]
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = var.allowed_ports[2]
+    to_port     = var.allowed_ports[2]
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = var.allowed_ports[3]
+    to_port     = var.allowed_ports[3]
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "EC2SecurityGroup"
+    Environment = var.Environment
+  }
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami             = var.instance_AMI
+  instance_type   = var.instance_type
+  subnet_id       = aws_subnet.public.id
+  security_group  = [aws_security_group.ec2_sg.id]
+  key_name        = var.key_name
+  user_data       = var.user_data
+
+  tags = {
+    Name        = "EC2Instance"
+    Environment = var.Environment
+  }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.ec2_instance.public_ip
+}
+
+output "ec2_ami" {
+  value = aws_instance.ec2_instance.ami
+}
+
+output "ec2_type" {
+  value = aws_instance.ec2_instance.instance_type
+}
+
+output "public_vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "ec2_subnet_id" {
+  value = aws_subnet.public.id
+}
+
+output "public_subnet_AZ" {
+  value = aws_subnet.public.availability_zone
+}
+
+output "ec2_region" {
+  value = var.Region
+}
