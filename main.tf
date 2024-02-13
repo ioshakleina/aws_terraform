3b25135ca54f8f6ab9ad2ce9013c8ae68beb263e
ioshakleina <ioshakleina@gmail.com>
2024-02-13 23:26:50 +0200

    one_solution

--- main.tf
+++ main.tf
@@ -112,31 +112,3 @@ resource "aws_instance" "ec2_instance" {
     Environment = var.Environment
   }
 }
-
-output "ec2_public_ip" {
-  value = aws_instance.ec2_instance.public_ip
-}
-
-output "ec2_ami" {
-  value = aws_instance.ec2_instance.ami
-}
-
-output "ec2_type" {
-  value = aws_instance.ec2_instance.instance_type
-}
-
-output "public_vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "ec2_subnet_id" {
-  value = aws_subnet.public.id
-}
-
-output "public_subnet_AZ" {
-  value = aws_subnet.public.availability_zone
-}
-
-output "ec2_region" {
-  value = var.Region
-}
